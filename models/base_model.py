1b823d6d216e005af8a5386a1fa86c03d8fea8d2
Aed10 <eddyyanyayoub@gmail.com>
2023-12-10 00:25:20 +0100

    Refactor base_model.py to include a shebang and docstrings for class and methods

--- models/base_model.py
+++ models/base_model.py
@@ -1,29 +1,29 @@
+#!/usr/bin/python3
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
 
-
 class BaseModel:
-    """BaseModel class"""
-
-    def __init__(self):
-        """init method"""
+    """A base class for all hbnb models"""
+    def __init__(self, *args, **kwargs):
+        """Instatntiates a new model"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
+    
     def __str__(self):
-        """str method"""
-        return "[{}] ({}) {}".format(self.__class__.__name__,
-                                     self.id, self.__dict__)
-
+        """Returns a string representation of the instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+    
     def save(self):
-        """save method"""
+        """Updates updated_at with the current datetime"""
         self.updated_at = datetime.now()
-
+    
     def to_dict(self):
-        """to_dict method"""
-        new_dict = self.__dict__.copy()
-        new_dict["__class__"] = self.__class__.__name__
-        new_dict["created_at"] = self.created_at.isoformat()
-        new_dict["updated_at"] = self.updated_at.isoformat()
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        new_dict = dict(self.__dict__)
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at.isoformat()
+        new_dict['updated_at'] = self.updated_at.isoformat()
         return new_dict
+
